34404352014c5475310382e2c9089af3fe77b7e9
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-03-10 12:07:09 -0500

    Add type signatures to GamePage Halogen code

--- src/GamePage.purs
+++ src/GamePage.purs
@@ -9,29 +9,36 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
+type State = Int
+
+data Action = Increment | Decrement
+
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
 
-data Action = Increment | Decrement
 
-component =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
-    }
-  where
-  initialState _ = 0
-
-  render state =
+render :: forall w. State -> HH.HTML w Action
+render state =
     HH.div_
       [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
       , HH.div_ [ HH.text $ show state ]
       , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
       ]
 
-  handleAction = case _ of
-    Increment -> H.modify_ \state -> state + 1
-    Decrement -> H.modify_ \state -> state - 1
+handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+  Increment -> H.modify_ \state -> state + 1
+  Decrement -> H.modify_ \state -> state - 1
+
+initialState :: Unit -> Int
+initialState _ = 0
+
+component :: forall output m t. H.Component t Unit output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
