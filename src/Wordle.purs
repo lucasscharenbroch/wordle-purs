47f3155d4ec67f5d753b8f869484f9fc2e03a2c7
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-03-16 12:01:58 -0500

    Optimize constraint functions (in solving)

--- src/Wordle.purs
+++ src/Wordle.purs
@@ -61,26 +61,29 @@ gradeGuess correct given = zipWith (\letter color -> {letter, color}) given $ co
 wordsFittingBoard :: Array String -> Board -> Array String
 wordsFittingBoard words board = filter fitsConstraints words
   where
-    fitsConstraints s = fitsPosConstraints board s && fitsCntConstraints board s
+    fitsPos = fitsPosConstraints board
+    fitsCnts = fitsCntConstraints board
+    fitsConstraints s =  fitsPos s  && fitsCnts s
 
 -- yellows => net minimum char count
 -- grays => net maximum char count
 fitsCntConstraints :: Board -> String -> Boolean
-fitsCntConstraints board s = Map.intersection minCnts guessCnts == minCnts
-                          && Foldable.all id (Map.intersectionWith (<=) minCnts guessCnts)
-                          && Foldable.all id (Map.intersectionWith (>=) maxCnts guessCnts')
+fitsCntConstraints board = _fitsCntConstraints
   where
     minCnts = foldl (Map.unionWith max) Map.empty <<< map rowMinCnts $ board
     maxCnts = foldl Map.union Map.empty $ map rowMaxCnts $ board
     rowToColorCountMap color = countIntoMap <<< map (\c -> c.letter) <<< filter (\c -> c.color == color)
     rowMinCnts = rowToColorCountMap Yellow
     rowMaxCnts row = mapWithKey (\c _ -> lookupOr 0 c yellows + lookupOr 0 c greens) grays
-      where grays = rowToColorCountMap Gray $ row
-            yellows = rowToColorCountMap Yellow $ row
-            greens = rowToColorCountMap Green $ row
-    guessCnts = countIntoMap <<< toCharArray $ s
-    guessCnts' = foldl (\m c -> Map.insertWith max c 0 m) guessCnts <<< toCharArray $ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
+      where
+        grays = rowToColorCountMap Gray $ row
+        yellows = rowToColorCountMap Yellow $ row
+        greens = rowToColorCountMap Green $ row
+    _fitsCntConstraints s = Map.intersection minCnts guessCnts == minCnts
+                         && Foldable.all id (Map.intersectionWith (<=) minCnts guessCnts)
+                         && Foldable.all id (Map.intersectionWith (>=) maxCnts guessCnts)
+      where
+        guessCnts = countIntoMap <<< toCharArray $ s
 
 data CurriedEqualityCheck a = CurriedEq a
                             | CurriedNe a
@@ -94,7 +97,7 @@ applyCurriedEqualityCheck (CurriedNe x) y = x /= y
 -- greens => exact position match
 -- yellows, grays => position mismatch
 fitsPosConstraints :: Board -> String -> Boolean
-fitsPosConstraints board s = all id $ zipWith ($) charFns (toCharArray s)
+fitsPosConstraints board = \s -> all id $ zipWith ($) charFns (toCharArray s)
   where
     charFns = map conjChecks charChecks
     conjChecks checks x = all (flip ($) x) checks'
